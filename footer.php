baf21ebb4f46449175856efd15cd832d3084f2cf
James Johnson <james@mindsharelabs.com>
2019-02-22 16:14:38 -0700

    fixed footer alignment and added link to mindshare.

--- footer.php
+++ footer.php
@@ -1,15 +1,16 @@
-			<!-- footer -->
-			<footer class="bottom-footer container-fluid" role="contentinfo">
-                <div class="row">
-                    <div class="col">
-                        <p class="align-middle"> &copy; <?php echo date('Y'); ?> Copyright <?php bloginfo('name'); ?>.</p>
-                    </div>
-                    <div class="col">
-                        <img src="<?php echo get_template_directory_uri() . '/img/mindshare.svg'; ?>" title="Hand Crafted by Mindshare Labs, Inc" alt="Mindshare Labs, Inc">
-                    </div>
-                </div>
+		<footer class="bottom-footer container mt-2" role="contentinfo">
+        <div class="row">
+          <div class="col-12 col-md-9 my-auto">
+            <p class="mb-0"> &copy; <?php echo date('Y'); ?> Copyright <?php bloginfo('name'); ?>.</p>
+          </div>
+          <div class="col-8 offset-2 offset-md-0 col-md mb-4 my-auto">
+						<small>HAND CRAFTED BY:</small>
+						<a href="https://mind.sh/are">
+	            <img src="<?php echo get_template_directory_uri() . '/img/mindshare.svg'; ?>" title="Hand Crafted by Mindshare Labs, Inc" alt="Mindshare Labs, Inc">
+						</a>
+          </div>
+        </div>
 			</footer>
-			<!-- /footer -->
 
 		</div>
 		</div>

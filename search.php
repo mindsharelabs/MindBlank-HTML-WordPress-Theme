4d39c47a1a35d8387d5f9115d23ac4582ea81f6c
James Johnson <james@mindsharelabs.com>
2019-03-20 11:31:56 -0600

    Fixed layout of search tempalte

--- search.php
+++ search.php
@@ -1,73 +1,55 @@
-<?php get_header();
-include 'layout/top-header.php';
-include 'layout/brand.php';
+<?php
+get_header();
+include 'layout/navigation.php';
+include 'layout/search-header.php';
+$s_query = get_search_query();
 ?>
-    <main role="main" aria-label="Content" class="container">
-        <div class="row">
-            <div class="col-xs-12 col-md-9">
-                <section class="container blog">
-                    <div class="row">
-                        <div class="col">
-                           
-                            <div class="row">
-                                <?php
-                                $types = array('product', 'page', 'posts');
-
-                                foreach ($types as $type) :
-
-                                    $posts = new WP_Query(
-                                        array(
-                                            's' => $s_query,            // search query
-                                            'post_type' => $type,
-                                            'posts_per_page' => -1,     // posts per page
-                                        )
-                                    );
-
-                                    if ($posts->have_posts()) :
-                                      $found = true;
-                                        $post_type = get_post_type_object($type);
-
-                                        $post_type_label = $post_type->labels->singular_name;
-
-
-                                        echo '<div class="row"><div class="col"><h3 class="search-label">';
-                                              echo sprintf('%s ' . $post_type_label . ' Results for ', $posts->found_posts) . $s_query;
-                                        echo '</h3></div></div>';
-
-
-                                        echo '<div class="row">';
-                                          while($posts->have_posts()) : $posts->the_post();
-                                            get_template_part('loop');
-                                          endwhile;
-                                        echo '</div>';
-
-                                    endif; //End if Have Posts
-
-                                endforeach; //End foreach post type
-
-                                if(!$found){
-                                  echo '<div class="row"><div class="col"><h3 class="search-label">';
-                                      echo sprintf('%s Results for ', $posts->found_posts) . $s_query;
-                                  echo '</h3></div></div>';
-                                }
-                                
-                                
-                                
-                                
-                                
-                                
-                                ?>
-
-                            </div>
-                        </div>
-                    </div>
-                </section>
-            </div>
-            <?php get_sidebar(); ?>
+  <main role="main" aria-label="Content" class="container">
+    <section class="container blog">
+      <div class="row">
+        <div class="col-12 col-md-8 offset-md-2">
+          <?php get_search_form(); ?>
         </div>
-        <!-- /section -->
-    </main>
+      </div>
+      <div class="row">
+              <?php
+              $types = array('staff', 'page', 'document'); //add all post types here
+              foreach ($types as $type) :
+                $found = false;
+                $posts = new WP_Query(
+                  array(
+                    's' => $s_query,            // search query
+                    'post_type' => $type,
+                    'posts_per_page' => -1,     // posts per page
+                  )
+                );
+
+                if ($posts->have_posts()) :
+                  $found = true;
+                  $post_type = get_post_type_object($type);
+                  $post_type_label = $post_type->labels->singular_name;
+
+                  echo '<div class="col-12"><h3 class="search-label"><hr>';
+                    echo sprintf('%s ' . $post_type_label . ' result(s) for ', $posts->found_posts) . '"' . $s_query . '"';
+                  echo '</h3></div>';
+                  while($posts->have_posts()) : $posts->the_post();
+                    get_template_part('loop-' . $type); //make sure there is a file named loop-[post type].php that gets used for all archive templates.
+                  endwhile;
+                endif; //End if Have Posts
+
+              endforeach; //End foreach post type
+
+              if(!$found){
+                echo '<div class="col"><h3 class="search-label">';
+                  echo sprintf('%s Results for ', $posts->found_posts) . '"' . $s_query . '"';
+                echo '</h3></div>';
+              }
+              ?>
+      </div>
+    </section>
+  </main>
 
 
-<?php include 'layout/top-footer.php';
+<?php
+include 'layout/top-footer.php';
 get_footer();

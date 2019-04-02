4e2450ee23e616404d5700d473ac3660fd592f0d
James Johnson <james@mindsharelabs.com>
2019-04-02 14:43:44 -0600

    theme upgrades.

--- loop.php
+++ loop.php
@@ -4,36 +4,21 @@ if (has_post_thumbnail()) :
   $image_url = aq_resize($image, 300, 300, true);
 endif;
 ?>
-<!-- article -->
-<article id="post-<?php the_ID(); ?>" <?php post_class('col-12'); ?>>
-    <div class="loop-content">
-        <div class="content">
-            <!-- post image -->
-            <?php if(isset($image_url)) : ?>
-              <div class="post-image">
-                <img src="<?php echo $image_url; ?>" alt="<?php the_title_attribute(); ?>">
-              </div>
-            <?php endif; ?>
-            <!-- /post image -->
-
-            <!-- post title -->
-            <h3 class="section-title">
-                <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a>
-            </h3>
-            <!-- /post title -->
-
-                <!-- post details -->
-                <span class="posted-date">
-                    <time datetime="<?php echo get_the_date(); ?>"><?php echo get_the_date(); ?></time>
-		            </span>
-                <span class="author-name"><?php echo get_the_author(); ?></span>
-                <!-- /post details -->
-            <?php mindwp_excerpt(); ?>
-
-    <hr>
-
-      <a href="<?php the_permalink(); ?>" class="btn btn-block btn-primary abs-button">Read More</a>
-        </div>
+<article id="post-<?php the_ID(); ?>" <?php post_class('col-12 col-md-4 mb-3'); ?>>
+  <div class="card">
+    <?php if(isset($image_url)) : ?>
+      <img class="card-img-top" src="<?php echo $image_url; ?>" alt="<?php the_title_attribute(); ?>">
+    <?php endif; ?>
+    <div class="card-body">
+      <h3 class="section-title">
+        <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a>
+      </h3>
+      <span class="posted-date">
+        <time datetime="<?php echo get_the_date(); ?>"><?php echo get_the_date(); ?></time>
+		   </span>
+      <span class="author-name"><?php echo get_the_author(); ?></span>
+      <?php the_excerpt(); ?>
     </div>
+    <a href="<?php the_permalink(); ?>" class="btn btn-block btn-primary">Read More</a>
+  </div>
 </article>
-<!-- /article -->
--- single.php
+++ single.php
@@ -1,76 +1,44 @@
-<?php get_header();
+<?php
+get_header();
 include 'layout/top-header.php';
-include 'layout/brand.php';
-?>
-    <main role="main" aria-label="Content" <?php post_class('container'); ?>>
-        <div class="row">
-            <div class="col-md-10 offset-md-1 col-12">
-                <div class="row">
-                    <div class="col-2">
-                        <!-- post details -->
-                        <div class="date">
-                            <time datetime="<?php the_time('Y-m-d'); ?> <?php the_time('H:i'); ?>">
-                                <span class="month"><?php the_time('F'); ?></span>
-                                <span class="day"><?php the_time('j'); ?></span>
-                            </time>
-                        </div>
-
-                    </div>
-                    <div class="col-10">
-                        <h1>
-                            <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a>
-                        </h1>
-                        <span class="author"><?php _e('Published by ', 'mindblank'); ?><?php the_author_posts_link(); ?></span>
-                    </div>
-                </div>
-                <?php if (has_post_thumbnail()) : ?>
-                    <div class="row">
-                        <div class="col-12">
-                            <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>" class="single-thumb">
-                                <?php
-                                $image = get_the_post_thumbnail_url();
-                                $image_url = aq_resize($image, 1000, 300, true, true);
-                                ?>
-                                <img src="<?php echo $image_url; ?>"  title="<?php the_title_attribute(); ?>" alt="<?php the_title_attribute(); ?>">
-                            </a>
-                        </div>
-                    </div>
-                <?php endif; ?>
-            </div>
+if(have_posts()) : while(have_posts()) : the_post(); ?>
+  <main role="main" aria-label="Content" <?php post_class('container'); ?>>
+    <div class="row mt-3">
+      <div class="col-12 col-md-10 offset-md-1">
+        <h1 class="my-auto text-center"><a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h1>
+        <div class="date text-center w-100">
+            <time datetime="<?php the_time('Y-m-d'); ?> <?php the_time('H:i'); ?>">
+                <span class="month"><?php the_time('F'); ?></span>
+                <span class="day"><?php the_time('j'); ?></span>
+            </time>
         </div>
+      </div>
+    </div>
+    <hr class="space">
+    <section class="row">
+      <article id="post-<?php the_ID(); ?>" <?php post_class('col-12 col-md-10 offset-md-1'); ?>>
+        <?php
+        if (has_post_thumbnail()) :
+          echo'<div class="row">';
+            echo'<div class="col-12">';
+            $attr = array('echo' => false,);
+            $image_url = aq_resize(get_the_post_thumbnail_url(), 300, 400, true, true);
+              echo '<a href="' . get_the_permalink() . '" title="' . the_title_attribute($attr) . '" class="single-thumb">';
+                echo '<img src="' . $image_url . '"  title="' . the_title_attribute($attr) . '" alt="' . the_title_attribute($attr) . '">';
+              echo '</a>';
+            echo'</div>';
+          echo'</div>';
+        endif;
+        ?>
+        <?php the_content(); ?>
+        <p><?php the_tags(__('Tags: ', 'mindblank'), ', '); ?></p>
+        <p><?php _e('Categorised in: ', 'mindblank');the_category(', '); // Separated by commas ?></p>
         <hr>
-        <div class="row">
-            <div class="col-md-7 offset-md-1 col-12">
-              <?php if(have_posts()) : while(have_posts()) : the_post(); ?>
-                <!-- section -->
-                <section>
-                    <!-- article -->
-                    <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+        <?php edit_post_link('Edit this Post', '', '', null, 'btn btn-warning'); // Always handy to have Edit Post Links available ?>
+      </article>
+    </section>
+  </main>
+<?php endwhile; endif;
 
-                        <?php the_content(); ?>
-
-                        <p><?php the_tags(__('Tags: ', 'mindblank'), ', '); ?></p>
-
-                        <p><?php _e('Categorised in: ', 'mindblank');the_category(', '); // Separated by commas ?></p>
-
-                        <?php edit_post_link('Edit this Article', '', '', null, 'btn btn-warning'); // Always handy to have Edit Post Links available ?>
-
-
-
-                    </article>
-                    <!-- /article -->
-                </section>
-              <?php endwhile; endif; ?>
-            </div>
-            <?php get_sidebar(); ?>
-        </div>
-        <div class="row">
-            <div class="col-10 offset-1">
-                <?php comments_template(); ?>
-            </div>
-        </div>
-        <!-- /section -->
-    </main>
-
-<?php include 'layout/top-footer.php';
+include 'layout/top-footer.php';
 get_footer();

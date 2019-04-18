ec74e76660ced6fce1b78f1c7b2bf5fc3936e0c7
James W. Johnson <james@mindsharelabs.com>
2019-04-18 13:37:23 -0600

    removed search form from sidebar.

--- template-fullwidth.php
+++ template-fullwidth.php
@@ -1,38 +1,24 @@
 <?php
-//Template Name: Full Width Page
 get_header();
 include 'layout/top-header.php';
-include 'layout/brand.php';
 ?>
-    <main role="main" aria-label="Content" class="container">
-        <div class="row">
-            <div class="col-12">
-                <!-- section -->
-                <section>
-                    <h1><?php the_title(); ?></h1>
-                    <?php if (have_posts()): while (have_posts()) : the_post(); ?>
-                        <!-- article -->
-                        <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+<main role="main" aria-label="Content" class="container">
+    <div class="row">
+      <section class="col-12">
+        <h1><?php the_title(); ?></h1>
+        <?php if (have_posts()): while (have_posts()) : the_post(); ?>
+          <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+            <?php the_content(); ?>
+            <?php comments_template('', true); // Remove if you don't want comments ?>
+            <br class="clear">
+            <?php edit_post_link('Edit this Page', '', '', null, 'btn btn-warning'); // Always handy to have Edit Post Links available ?>
+          </article>
 
-                            <?php the_content(); ?>
-
-                            <?php comments_template('', true); // Remove if you don't want comments ?>
-
-                            <br class="clear">
-
-                            <?php edit_post_link(); ?>
-
-                        </article>
-                        <!-- /article -->
-                    <?php endwhile; ?>
-                    <?php endif; ?>
-
-                </section>
-            </div>
-
-        </div>
-        <!-- /section -->
-    </main>
-
-<?php include 'layout/top-footer.php';
+        <?php endwhile; endif; ?>
+      </section>
+    </div>
+  </div>
+</main>
+<?php
+include 'layout/top-footer.php';
 get_footer();

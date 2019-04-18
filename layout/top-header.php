5071318b82a975670b67c46dea4f7c1b8c4365cd
James W. Johnson <james@mindsharelabs.com>
2019-04-18 13:22:11 -0600

    theme updates and refinements.

--- layout/top-header.php
+++ layout/top-header.php
@@ -1,10 +1,10 @@
 <header class="header top-header" role="banner">
   <div class="container">
     <div class="row">
-        <div class="col-10">
+        <div class="col-10 col-md-4 offset-md-8">
             <?php get_search_form();?>
         </div>
-        <div class="col-2">
+        <div class="col-2 d-block d-md-none">
             <span class="menu-toggle"><i class="far fa-bars"></i></span>
         </div>
     </div>

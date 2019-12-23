0578b0a91746fc95d737927a3daaac738486d1b6
James Johnson <james@mindsharelabs.com>
2019-12-23 01:32:01 -0700

    one last adjustment.

--- layout/top-header.php
+++ layout/top-header.php
@@ -1,13 +1,13 @@
 <header class="header top-header" role="banner">
   <div class="container pt-2">
     <div class="row">
-        <div class="col-9 my-auto d-none d-md-block ">
+        <div class="col my-auto d-none d-md-block ">
           <nav class="desktop header-menu text-right">
             <?php mindblank_nav('header-menu'); ?>
           </nav>
         </div>
-        <div class="col text-right my-auto">
-          <span class="menu-toggle d-inline d-md-none"><i class="fas fa-bars"></i></span>
+        <div class="col-3 text-right my-auto d-block d-md-none">
+          <span class="menu-toggle"><i class="fas fa-bars"></i></span>
         </div>
     </div>
   </div>

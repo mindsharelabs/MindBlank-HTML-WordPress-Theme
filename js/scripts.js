1e54142909079b82f1a78da274f1c848977632e9
James W. Johnson <me@thejameswilliam.com>
2020-11-11 10:14:59 -0700

    removed slideout.

--- js/scripts.js
+++ js/scripts.js
@@ -15,39 +15,7 @@
 					var menuWidth = 400;
 				}
 
-        var slideout = new Slideout({
-            'panel': document.getElementById('main-panel'),
-            'menu': document.getElementById('main-nav'),
-            'padding': menuWidth,
-            'tolerance': 0,
-            'side': 'right'
-        });
-
-        document.querySelector('.slideout-menu').style.width = windowWidth + 'px';
-
-        //Toggle button
-        document.querySelector('.menu-toggle').addEventListener('click', function () {
-            slideout.toggle();
-        });
-				document.querySelector('.menu-toggle-close').addEventListener('click', function () {
-            slideout.close();
-        });
-
-
-        slideout.on('beforeclose', function () {
-            $('.slideout-menu').fadeOut();
-        });
-        slideout.on('beforeopen', function () {
-            $('.slideout-menu').fadeIn();
-        });
-        slideout.on('open', function () {
-            $('.menu-toggle svg').addClass("fa-window-close").removeClass("fa-bars");
-
-        });
-        slideout.on('close', function () {
-            $('.menu-toggle svg').addClass("fa-bars ").removeClass("fa-window-close");
-
-        });
+        
 
         jQuery('body').addClass('fade-in');
 

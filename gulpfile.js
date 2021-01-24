0d898392e9fb569919637e23c86aeb6fa6bc897b
James W. Johnson <me@thejameswilliam.com>
2021-01-24 14:11:35 -0700

    re-arranged compiled css and added source maps.

--- gulpfile.js
+++ gulpfile.js
@@ -1,42 +1,45 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
+const sourcemaps = require('gulp-sourcemaps');
 const del = require('del');
 
 gulp.task('styles', () => {
     return gulp.src('sass/style.scss')
-        .pipe(sass({
-          includePaths: ['sass/**/*.scss'],
-          outputStyle: 'compressed'
-        }).on('error', sass.logError))
-        .pipe(gulp.dest('.'))
+      .pipe(sourcemaps.init())
+      .pipe(sass({
+        includePaths: ['sass/**/*.scss'],
+        outputStyle: 'compressed'
+      }).on('error', sass.logError))
+      .pipe(sourcemaps.write('.'))
+      .pipe(gulp.dest('./css/'))
 });
+
 
 gulp.task('block-styles', () => {
     return gulp.src('sass/block-styles.scss')
-        .pipe(sass({
-          outputStyle: 'compressed'
-        }).on('error', sass.logError))
-        .pipe(gulp.dest('./css/'))
+      .pipe(sourcemaps.init())
+      .pipe(sass({
+        outputStyle: 'compressed'
+      }).on('error', sass.logError))
+      .pipe(sourcemaps.write('.'))
+      .pipe(gulp.dest('./css/'))
 });
 
 gulp.task('slick-styles', () => {
     return gulp.src('sass/slick.scss')
-        .pipe(sass({
-          outputStyle: 'compressed'
-        }).on('error', sass.logError))
-        .pipe(gulp.dest('./css/'))
+      .pipe(sourcemaps.init())
+      .pipe(sass({
+        outputStyle: 'compressed'
+      }).on('error', sass.logError))
+      .pipe(sourcemaps.write('.'))
+      .pipe(gulp.dest('./css/'))
 });
 
-gulp.task('clean', () => {
-    return del([
-        'style.css',
-    ]);
-});
 
 gulp.task('watch', () => {
-    gulp.watch('sass/**/*.scss', (done) => {
-        gulp.series(['clean', 'styles', 'block-styles', 'slick-styles'])(done);
-    });
+  gulp.watch('sass/**/*.scss', (done) => {
+    gulp.series(['styles', 'block-styles', 'slick-styles'])(done);
+  });
 });
 
 gulp.task('default', gulp.series(['watch']));

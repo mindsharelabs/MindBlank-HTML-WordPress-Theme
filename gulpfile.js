70e4a99c3169ee4df46c9933fe8e723ad1adb3e9
James W Johnson <me@thejameswilliam.com>
2025-08-26 20:37:05 -0600

    updated bootstrap and sass compiler. Turned off deprication warnings. We cannot migrate to @use because bootstrap is not built with this in mind. Migrating to @use creates a dependancy loop between functions and variables causing a compile error.

--- gulpfile.js
+++ gulpfile.js
@@ -1,55 +1,101 @@
 import gulp from "gulp";
 
-import dartSass from 'sass'
-import gulpSass from 'gulp-sass'
+// Silence Dart Sass deprecations globally (affects this process)
+if (!process.env.SASS_SILENCE_DEPRECATIONS) {
+  // You can replace 'all' with a comma-separated list of IDs if you prefer fine-grained control
+  process.env.SASS_SILENCE_DEPRECATIONS = 'all';
+}
+
+import dartSass from "sass";
+import gulpSass from "gulp-sass";
 const sass = gulpSass(dartSass);
 
-import sourcemaps from 'gulp-sourcemaps';
-import {deleteAsync} from 'del';
+import sourcemaps from "gulp-sourcemaps";
+import { deleteAsync } from "del";
 
-gulp.task('theme-styles', () => {
-    return gulp.src('sass/styles.scss')
-      .pipe(sourcemaps.init())
-      .pipe(sass({
-        outputStyle: 'compressed'//nested, expanded, compact, compressed
-      }).on('error', sass.logError))
-      .pipe(sourcemaps.write('.'))
-      .pipe(gulp.dest('./css/'))
-});
+// ----------------------------------------------
+// Common settings
+// ----------------------------------------------
+const SASS_OPTIONS = {
+  outputStyle: "compressed", // nested, expanded, compact, compressed
+  includePaths: ["node_modules"], // allow @import "bootstrap/..." etc.
+  quietDeps: true,              // hide warnings from dependencies
+  silenceDeprecations: [
+    "import",          // using @import instead of @use/@forward
+    "global-builtin",  // using global built-ins like unquote()
+    "legacy-js-api"     // older plugin interfaces that may warn
+  ],
+};
 
-gulp.task('editor-styles', () => {
-    return gulp.src('sass/editor-styles.scss')
-      .pipe(sourcemaps.init())
-      .pipe(sass({
-        outputStyle: 'compressed'//nested, expanded, compact, compressed
-      }).on('error', sass.logError))
-      .pipe(sourcemaps.write('.'))
-      .pipe(gulp.dest('./css/'))
-});
+const PATHS = {
+  src: {
+    theme: "sass/styles.scss",
+    editor: "sass/editor-styles.scss", 
+    notFound: "sass/404-styles.scss",
+    all: "sass/**/*.scss",
+  },
+  dest: { 
+    css: "./css/",
+  },
+  clean: [
+    "css/*.css",
+    "css/*.css.map",
+  ],
+};
 
-gulp.task('404-styles', () => {
-    return gulp.src('sass/404-styles.scss')
-      .pipe(sourcemaps.init())
-      .pipe(sass({
-        outputStyle: 'compressed'//nested, expanded, compact, compressed
-      }).on('error', sass.logError))
-      .pipe(sourcemaps.write('.'))
-      .pipe(gulp.dest('./css/'))
-});
+// ----------------------------------------------
+// Compile tasks
+// ----------------------------------------------
+export const themeStyles = () =>
+  gulp
+    .src(PATHS.src.theme, { allowEmpty: true })
+    .pipe(sourcemaps.init())
+    .pipe(sass(SASS_OPTIONS).on("error", sass.logError))
+    .pipe(sourcemaps.write("."))
+    .pipe(gulp.dest(PATHS.dest.css));
 
-gulp.task('clean', () => {
-    return deleteAsync([
-        'inc/css/block-style.css',
-        'inc/css/editor-styles.css',
-        'inc/css/404-styles.css',
-        'inc/css/style.css',
-    ]);
-}); 
+export const editorStyles = () =>
+  gulp
+    .src(PATHS.src.editor, { allowEmpty: true })
+    .pipe(sourcemaps.init())
+    .pipe(sass(SASS_OPTIONS).on("error", sass.logError))
+    .pipe(sourcemaps.write("."))
+    .pipe(gulp.dest(PATHS.dest.css));
 
-gulp.task('watch', () => {
-  gulp.watch('sass/*.scss', (done) => {
-    gulp.series(['theme-styles'])(done);
-  });
-});
+export const notFoundStyles = () =>
+  gulp
+    .src(PATHS.src.notFound, { allowEmpty: true })
+    .pipe(sourcemaps.init())
+    .pipe(sass(SASS_OPTIONS).on("error", sass.logError))
+    .pipe(sourcemaps.write("."))
+    .pipe(gulp.dest(PATHS.dest.css));
 
-gulp.task('default', gulp.series(['theme-styles', 'editor-styles', '404-styles', 'watch']));
+// ----------------------------------------------
+// Clean
+// ----------------------------------------------
+export const clean = () => deleteAsync(PATHS.clean);
+
+// ----------------------------------------------
+// Watch
+// ----------------------------------------------
+export const watchScss = () => {
+  gulp.watch(PATHS.src.all, gulp.series(
+    themeStyles, 
+    editorStyles, 
+    notFoundStyles,
+  ));
+};
+
+// ----------------------------------------------
+// Composite tasks
+// ----------------------------------------------
+export const build = gulp.series(
+  clean,
+  gulp.parallel(
+    themeStyles,
+    editorStyles,
+    notFoundStyles,
+  )
+);
+
+export default gulp.series(build, watchScss);
